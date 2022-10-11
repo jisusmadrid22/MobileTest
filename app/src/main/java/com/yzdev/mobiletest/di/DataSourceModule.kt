921f4e940930031fff04e5ecbfb8fea36acfe2b2
jesus medina <jm9417174@gmail.com>
2022-10-10 22:40:31 -0500

    retrofit config v2

--- app/src/main/java/com/yzdev/mobiletest/di/DataSourceModule.kt
+++ app/src/main/java/com/yzdev/mobiletest/di/DataSourceModule.kt
@@ -1,4 +1,35 @@
 package com.yzdev.mobiletest.di
 
+import com.yzdev.mobiletest.dataSource.RestDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class DataSourceModule {
+
+    @Singleton
+    @Provides
+    @Named("BaseUrl")
+    fun provideBaseUrl() = "https://hn.algolia.com/api/v1/"
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(@Named("BaseUrl") baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun restDataSource(retrofit: Retrofit): RestDataSource = retrofit.create(RestDataSource::class.java)
 }
