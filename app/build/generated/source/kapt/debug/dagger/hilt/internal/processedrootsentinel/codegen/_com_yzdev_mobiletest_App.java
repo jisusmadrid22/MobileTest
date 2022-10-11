33a894803d2f64ea1d5e69fec70d86cda4c01610
jesus medina <jm9417174@gmail.com>
2022-10-10 22:21:18 -0500

    hilt config

---
+++ app/build/generated/source/kapt/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_yzdev_mobiletest_App.java
@@ -0,0 +1,12 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+/**
+ * This class should only be referenced by generated code!This class aggregates information across multiple compilations.
+ */
+@ProcessedRootSentinel(
+    roots = "com.yzdev.mobiletest.App"
+)
+public class _com_yzdev_mobiletest_App {
+}
--- app/src/main/java/com/yzdev/mobiletest/MainActivity.kt
+++ app/src/main/java/com/yzdev/mobiletest/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yzdev.mobiletest.presentation.screens.notice.NoticeLayout
 import com.yzdev.mobiletest.presentation.ui.theme.MobileTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
